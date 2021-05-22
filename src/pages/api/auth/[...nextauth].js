f217e2f0825a94170cb7afea39aaf50ca1b12eca
Sai Pranay <saipranayadepu07@gmail.com>
2021-05-22 11:59:44 +0530

    github auth

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -8,9 +8,13 @@ export default NextAuth({
       clientSecret: process.env.GOOGLE_SECRET,
     }),
   
-    Providers.Facebook({
-      clientId: process.env.FACEBOOK_ID,
-      clientSecret: process.env.FACEBOOK_SECRET,
+    // Providers.Facebook({
+    //   clientId: process.env.FACEBOOK_ID,
+    //   clientSecret: process.env.FACEBOOK_SECRET,
+    // }),
+    Providers.GitHub({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
     }),
     // add more hear
   ],
