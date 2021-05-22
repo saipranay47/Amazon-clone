c29ae89274b022b86310cde0382248f528e9207e
Sai Pranay <saipranayadepu07@gmail.com>
2021-05-22 18:52:13 +0530

    hover update

--- src/components/Hover.js
+++ src/components/Hover.js
@@ -5,7 +5,6 @@ import { selectItems, selectTotal } from "../slices/basketSlice";
 import { useSession } from "next-auth/client";
 import { groupBy } from "lodash";
 
-
 function Hover() {
   const items = useSelector(selectItems);
   const total = useSelector(selectTotal);
@@ -15,19 +14,28 @@ function Hover() {
   return (
     <div>
       <div className="flex flex-col">
-        <h1
-          className={`text-xs "border-b "pb-2"}`}
-        >
-          {items.length === 0
-            ? "Your Amazon Basket is empty."
-            : ""}
+        <h1 className={`text-xs "border-b "pb-2"}`}>
+          {items.length === 0 ? "Your Amazon Basket is empty." : ""}
         </h1>
 
         {groupedItems.map((group, i) => (
-            <div className="m-4 flex justify-items-center">
-          <Image src={group[0].image} width={100} height={100} objectFit="contain" />
+          <div className="m-4 flex justify-items-center">
+            <Image
+              src={group[0].image}
+              width={100}
+              height={100}
+              objectFit="contain"
+            />
           </div>
         ))}
+        <h2 className="whitespace-nowrap">
+          
+          Subtotal ({items.length} items):{" "}
+          <span className="font-bold">
+            <Currency quantity={total * 103.47} currency="INR" />
+          </span>
+
+        </h2>
       </div>
     </div>
   );
