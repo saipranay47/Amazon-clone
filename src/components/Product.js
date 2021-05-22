2b660d394fa2bc989865a4d7c65d47eebcc2814b
Sai Pranay <saipranayadepu07@gmail.com>
2021-05-22 09:59:48 +0530

    popup

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,9 +1,26 @@
 import { StarIcon } from "@heroicons/react/solid";
 import Image from "next/image";
-import { useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import Currency from "react-currency-formatter";
 import { useDispatch } from "react-redux";
-import { addToBasket } from "../slices/basketSlice";
+import { addToBasket, removeFromBasket } from "../slices/basketSlice";
+
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Snackbar from '@material-ui/core/Snackbar';
+import IconButton from '@material-ui/core/IconButton';
+// import CloseIcon from '@material-ui/icons/Close';
+import {
+ 
+  XIcon,
+ 
+} from "@heroicons/react/outline";
+
+const useStyles = makeStyles((theme) => ({
+  close: {
+    padding: theme.spacing(0.5),
+  },
+}));
 
 const MAX_RATING = 5;
 const MIN_RATING = 1;
@@ -30,8 +47,52 @@ function Product({ id, title, price, description, category, image }) {
 
     dispatch(addToBasket(product));
   };
+
+    const [snackPack, setSnackPack] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [messageInfo, setMessageInfo] = useState(undefined);
+
+  useEffect(() => {
+    if (snackPack.length && !messageInfo) {
+      // Set a new snack when we don't have an active one
+      setMessageInfo({ ...snackPack[0] });
+      setSnackPack((prev) => prev.slice(1));
+      setOpen(true);
+    } else if (snackPack.length && messageInfo && open) {
+      // Close an active snack when a new one is added
+      setOpen(false);
+    }
+  }, [snackPack, messageInfo, open]);
+
+  const handleClick = (message) => () => {
+
+    addItemsToBasket();
+
+    setSnackPack((prev) => [...prev, { message, key: new Date().getTime() }]);
+    
+  };
+
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setOpen(false);
+  };
+
+  const handleExited = () => {
+    setMessageInfo(undefined);
+  };
+
+
+    const RemoveItemFromBasket = () => {
+
+    dispatch(removeFromBasket({id}));
+    handleClose();
+
+  };
+  const classes = useStyles();
   return (
-    <div className="relative flex flex-col m-5 bg-white z-30 p-10">
+    <div className="relative flex flex-col m-5 bg-white p-10">
       <p className="absolute top-2 right-2 text-xs italic text-gray-400">
         {category}
       </p>
@@ -65,9 +126,41 @@ function Product({ id, title, price, description, category, image }) {
         </div>
       )}
 
-      <button onClick={addItemsToBasket} className="mt-auto button">
+      {/* <button onClick={addItemsToBasket} className="mt-auto button">
         Add to Basket
-      </button>
+      </button> */}
+
+      <button className="mt-auto button" onClick={handleClick(`${title} -- Added to basket`)}>Add to Basket</button>
+      <Snackbar
+        style={{zIndex:"10000"}}
+        key={messageInfo ? messageInfo.key : undefined}
+        
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'left',
+        }}
+        open={open}
+        autoHideDuration={60000}
+        onClose={handleClose}
+        onExited={handleExited}
+        message={messageInfo ? messageInfo.message : undefined}
+        action={
+          <Fragment style={{zIndex:"10000"}} className="bg-green-400">
+            <Button color="secondary" size="small" onClick={RemoveItemFromBasket} >
+              Remove
+            </Button>
+            <IconButton
+              aria-label="close"
+              color="inherit"
+              className={classes.close}
+              onClick={handleClose}
+            >
+              <XIcon className="h-8 text-white cursor-pointer" />
+            </IconButton>
+          </Fragment>
+        }
+      />
+
     </div>
   );
 }
