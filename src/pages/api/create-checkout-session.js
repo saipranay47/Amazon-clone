3f0dee82e0eb8be186c2270d73a594982ee43a13
Sai Pranay <saipranayadepu07@gmail.com>
2021-05-23 08:25:27 +0530

    small change

--- src/pages/api/create-checkout-session.js
+++ src/pages/api/create-checkout-session.js
@@ -1,37 +1,47 @@
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+import { groupBy } from "lodash";
 
-export default async (req,res) =>{
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+export default async (req, res) => {
     const { items, email } = req.body;
 
-    const transformedItems = items.map((item) => ({
-        description:item.description,
-        quantity:1,
-        price_data:{
-            currency:'inr',
-            unit_amount:item.price * 100 * 73,
-            product_data:{
-                name:item.title,
-                images: [item.image],
-            },
+    console.log(items);
+    console.log(email);
 
+    // @Todo : READ THE STRIPE DOCS!
+
+    const groupedItems = Object.values(groupBy(items, "id"));
+
+    const transformedItems = groupedItems.map((group) => ({
+        description: group[0].description,
+        quantity: group.length,
+        price_data: {
+            currency: "inr",
+            unit_amount: group[0].price * 100 *73, 
+            product_data: {
+                name: group[0].title,
+                images: [group[0].image],
+            },
         },
     }));
 
     const session = await stripe.checkout.sessions.create({
-        payment_method_types:["card"],
-        shipping_rates:['shr_1Iu6iBSBHfL9pzhwFC546tT0'],
-        shipping_address_collection:{
-            allowed_countries:["GB", "US","CA","IN"]
+        payment_method_types: ["card"],
+        shipping_rates: ["shr_1Iu6iBSBHfL9pzhwFC546tT0"], // Created fees in Stripe's dashboard
+        shipping_address_collection: {
+            allowed_countries: ["GB", "US", "CA", "FR"], // RTFM!
         },
-        line_items:transformedItems,
-        mode:"payment",
-        success_url:`${process.env.HOST}/success`,
-        cancel_url:`${process.env.HOST}/checkout`,
-        metadata:{
+        line_items: transformedItems,
+        mode: "payment",
+        success_url: `${process.env.HOST}/success`,
+        cancel_url: `${process.env.HOST}/checkout`,
+        metadata: {
             email,
-            images:JSON.stringify(items.map((item) => item.image)),
+            images: JSON.stringify(items.map((item) => item.image)),
         },
     });
 
-    res.status(200).json({ id: session.id});
-}
+    console.log("session created!", session.id);
+
+    res.status(200).json({ id: session.id });
+};
